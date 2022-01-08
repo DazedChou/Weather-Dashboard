b0396bc88f40fa2d6dc3059dde119998e0f53b59
David Chou <davidchou2009@gmail.com>
2022-01-08 08:24:09 -0800

    added fetch functionality

---
+++ script.js
@@ -0,0 +1,49 @@
+
+//apikey = e6a41f6fdcb53d621e32978ad90ef82f
+
+
+$('.btn').on('click', function () {
+  var city = $('#cityText').val();
+  city = city.replace(" ", "+");
+  var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=e6a41f6fdcb53d621e32978ad90ef82f';
+  console.log(city);
+  console.log("requestUrl: " + requestUrl);
+
+
+  //fetch weather
+  fetch(requestUrl)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    //display weather
+    var cityName = $('<h2>').text(data.name);
+    $('.weather').append(cityName)
+    var currentDate = moment().format("M/DD/YYYY");
+    $('.weather').append(currentDate);
+  })
+
+
+
+})
+
+
+
+// GIVEN a weather dashboard with form inputs
+// WHEN I search for a city
+// THEN I am presented with current and future conditions
+// for that city and that city is added to the search history
+// WHEN I view current weather conditions for that city
+// THEN I am presented with the city name, the date, an
+// icon representation of weather conditions, the temperature, the humidity,
+// the wind speed, and the UV index
+// WHEN I view the UV index
+// THEN I am presented with a color that indicates whether
+// the conditions are favorable, moderate, or severe
+// WHEN I view future weather conditions for that city
+// THEN I am presented with a 5-day forecast that displays the
+// date, an icon representation of weather conditions, the temperature,
+// and the humidity
+// WHEN I click on a city in the search history
+// THEN I am again presented with current and future conditions for that city
