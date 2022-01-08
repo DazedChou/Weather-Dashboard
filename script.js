8adc1d690965c5c97e19a8a3a56d78d77e45d3d9
David Chou <davidchou2009@gmail.com>
2022-01-08 11:01:52 -0800

    display city name, date, weather conditions temperature, humidity, wind speed, and uv index when searching for city

--- script.js
+++ script.js
@@ -5,24 +5,45 @@
 $('.btn').on('click', function () {
   var city = $('#cityText').val();
   city = city.replace(" ", "+");
-  var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=e6a41f6fdcb53d621e32978ad90ef82f';
+  var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&appid=e6a41f6fdcb53d621e32978ad90ef82f';
+
   console.log(city);
-  console.log("requestUrl: " + requestUrl);
+  console.log(requestUrl)
 
 
   //fetch weather
   fetch(requestUrl)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-    //display weather
-    var cityName = $('<h2>').text(data.name);
-    $('.weather').append(cityName)
-    var currentDate = moment().format("M/DD/YYYY");
-    $('.weather').append(currentDate);
-  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      //display weather
+      var currentDate = moment().format("(M/DD/YYYY)");
+
+      $('#city').text(data.name + " " + currentDate);
+      $('#temp').text("Temperature: " + data.main.temp + " F")
+      $('#humidity').text("Humidity: " + data.main.humidity + "%")
+      $('#windspeed').text("Wind Speed: " + data.wind.speed + " MPH")
+
+      var lat = data.coord.lat;
+      var lon = data.coord.lon;
+      console.log("lat", lat);
+      console.log("lon", lon);
+
+      var uviUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&appid=e6a41f6fdcb53d621e32978ad90ef82f'
+      //fetch UV index using the lat and long
+      fetch(uviUrl)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          console.log(data);
+          $('#uvi').text("UV Index: " + data.current.uvi)
+
+        })
+
+    })
 
 
 
